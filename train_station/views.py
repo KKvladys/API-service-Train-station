9483cd96013aae4a28571e2628c48d91d180e062
Vladyslav Kucherenko <vlados.soc@gmail.com>
2024-12-25 17:47:24 +0200

    applying filters and add pagination

--- train_station/views.py
+++ train_station/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
-from rest_framework import viewsets
+from django_filters import rest_framework as filters
+from rest_framework import viewsets, mixins
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.viewsets import GenericViewSet
 
+from train_station.filters import (
+    StationFilter,
+    RouteFilter,
+    TripFilter
+)
 from train_station.models import (
     Station,
     Route,
@@ -12,39 +19,97 @@ from train_station.models import (
     Crew
 )
 from train_station.serializers import (
-    CrewSerializer, StationSerializer, RouteSerializer, TrainSerializer, OrderSerializer, TicketSerializer,
-    TripSerializer, TrainTypeSerializer
+    CrewSerializer,
+    StationSerializer,
+    RouteSerializer,
+    TrainSerializer,
+    OrderSerializer,
+    TicketSerializer,
+    TripSerializer,
+    TrainTypeSerializer,
+    RouteListSerializer,
+    TrainListSerializer,
+    OrderListSerializer,
+    TripListSerializer
 )
 
 
-class CrewViewSet(viewsets.ModelViewSet):
+class TripOrderViewPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = "page_size"
+    max_page_size = 10
+
+
+class CrewViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
     queryset = Crew.objects.all()
     serializer_class = CrewSerializer
 
 
-class StationViewSet(viewsets.ModelViewSet):
+class StationViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = StationFilter
     queryset = Station.objects.all()
     serializer_class = StationSerializer
 
 
 class RouteViewSet(viewsets.ModelViewSet):
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = RouteFilter
     queryset = Route.objects.all()
-    serializer_class = RouteSerializer
+
+    def get_serializer_class(self):
+        serializer_class = RouteSerializer
+        if self.action == "list":
+            return RouteListSerializer
+        return serializer_class
 
 
 class TrainViewSet(viewsets.ModelViewSet):
     queryset = Train.objects.all()
-    serializer_class = TrainSerializer
+
+    def get_serializer_class(self):
+        serializer_class = TrainSerializer
+        if self.action == "list":
+            return TrainListSerializer
+        return serializer_class
 
 
 class OrderViewSet(viewsets.ModelViewSet):
+    pagination_class = TripOrderViewPagination
     queryset = Order.objects.all()
-    serializer_class = OrderSerializer
+
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        serializer_class = OrderSerializer
+        if self.action == "list":
+            return OrderListSerializer
+        return serializer_class
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 
 class TripViewSet(viewsets.ModelViewSet):
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = TripFilter
+    pagination_class = TripOrderViewPagination
     queryset = Trip.objects.all()
-    serializer_class = TripSerializer
+
+    def get_serializer_class(self):
+        serializer_class = TripSerializer
+        if self.action == "list":
+            return TripListSerializer
+        return serializer_class
 
 
 class TicketViewSet(viewsets.ModelViewSet):
@@ -52,6 +117,10 @@ class TicketViewSet(viewsets.ModelViewSet):
     serializer_class = TicketSerializer
 
 
-class TrainTypeViewSet(viewsets.ModelViewSet):
+class TrainTypeViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet
+):
     queryset = TrainType.objects.all()
     serializer_class = TrainTypeSerializer

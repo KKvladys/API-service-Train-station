67917757648e39735589c60778f33330db48ca12
Vladyslav Kucherenko <vlados.soc@gmail.com>
2024-12-29 10:20:27 +0200

    change lifetime jwt

--- core/settings.py
+++ core/settings.py
@@ -159,7 +159,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=500),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1000),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
 }
